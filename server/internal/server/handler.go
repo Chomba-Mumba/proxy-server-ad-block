a2700a895b28984622570e3859c05f2100b632a2
chomba-mumba <chombaibex@gmail.com>
2025-12-11 22:40:32 +0000

    added tunneling handler

--- server/internal/server/handler.go
+++ server/internal/server/handler.go
@@ -3,9 +3,36 @@ package server
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"time"
 )
+
+func handleTunneling(w http.ResponseWriter, r *http.Request) {
+	dest_conn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	client_conn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	go transfer(dest_conn, client_conn)
+	go transfer(client_conn, dest_conn)
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
 
 func ProxyHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("[PROXY SERVER] request received at %s at %s \n forwading request...\n", r.URL, time.Now().UTC())
